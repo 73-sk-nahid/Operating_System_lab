8f9ace6c9afc1837f18cb678340d9a1aca7efd0b
Sk. Nahid <118644301+nahid2700@users.noreply.github.com>
2023-03-03 23:05:22 +0600

    Add files via upload

---
+++ Concatenate Two String.sh
@@ -0,0 +1,6 @@
+echo Here this will use for Concatenate two string
+echo Simply enter two string -------------
+read -p "Enter First String:" a
+read -p "Enter Second String:" b
+c = a + b
+echo After concatenation: $c
