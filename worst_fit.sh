4424bcb00549f4bb8a3f6e4ea3ab100fe887ea38
Sk. Nahid <118644301+nahid2700@users.noreply.github.com>
2023-06-15 22:13:02 +0600

    Worst Fit Memory Allocation

---
+++ worst_fit.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+max=25
+
+declare -a frag
+declare -a b
+declare -a f
+declare -a bf
+declare -a ff
+
+printf "Enter the number of blocks: "
+read nb
+
+printf "Enter the number of files: "
+read nf
+
+printf "Enter the size of the blocks:\n"
+for ((i=1; i<=nb; i++))
+do
+    printf "Block %d: " "$i"
+    read b[$i]
+done
+
+printf "Enter the size of the files:\n"
+for ((i=1; i<=nf; i++))
+do
+    printf "File %d: " "$i"
+    read f[$i]
+done
+
+for ((i=1; i<=nf; i++))
+do
+    highest=0
+    for ((j=1; j<=nb; j++))
+    do
+        if [[ ${bf[$j]} != 1 ]]
+        then
+            temp=$((b[$j] - f[$i]))
+            if [[ $temp -ge 0 ]]
+            then
+                if [[ $temp -gt $highest ]]
+                then
+                    ff[$i]=$j
+                    highest=$temp
+                fi
+            fi
+        fi
+    done
+    frag[$i]=$highest
+    bf[${ff[$i]}]=1
+done
+
+printf "\nFile_no \tFile_size \tBlock_no \tBlock_size \tFragment\n"
+for ((i=1; i<=nf; i++))
+do
+    printf "%d\t\t%d\t\t%d\t\t%d\t\t%d\n" "$i" "${f[$i]}" "${ff[$i]}" "${b[${ff[$i]}]}" "${frag[$i]}"
+done
+
