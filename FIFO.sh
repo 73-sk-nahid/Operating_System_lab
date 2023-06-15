686bbdfe3cc8891d5c83341d58e93cd30bf5ab1d
Sk. Nahid <118644301+nahid2700@users.noreply.github.com>
2023-06-15 23:01:24 +0600

    Update FIFO.sh

--- FIFO.sh
+++ FIFO.sh
@@ -1,53 +1,61 @@
 #!/bin/bash
 
-declare -a a
-declare -a frame
+pageFaultCount=0
+declare -a pages
+declare -a memory
+memoryIndex=0
 
-printf "Enter the number of pages: "
-read n
+printf "Enter number of pages: "
+read numberOfPages
 
-printf "Enter the page numbers:\n"
-for ((i = 1; i <= n; i++))
+printf "Enter the pages:\n"
+for ((i = 0; i < numberOfPages; i++))
 do
-    printf "Page %d: " "$i"
-    read a[$i]
+    read pages[$i]
 done
 
-printf "Enter the number of frames: "
-read no
+printf "Enter number of frames: "
+read numberOfFrames
 
-for ((i = 0; i < no; i++))
+for ((i = 0; i < numberOfFrames; i++))
 do
-    frame[$i]=-1
+    memory[$i]=-1
 done
 
-j=0
-printf "\nRef String\tPage Frames\n"
-for ((i = 1; i <= n; i++))
+printf "The Page Replacement Process is -->\n"
+for ((i = 0; i < numberOfPages; i++))
 do
-    printf "%d\t\t" "${a[$i]}"
-    avail=0
-
-    for ((k = 0; k < no; k++))
+    for ((j = 0; j < numberOfFrames; j++))
     do
-        if [ "${frame[$k]}" -eq "${a[$i]}" ]; then
-            avail=1
+        if [ ${memory[$j]} -eq ${pages[$i]} ]
+        then
             break
         fi
     done
 
-    if [ "$avail" -eq 0 ]; then
-        frame[$j]=${a[$i]}
-        j=$(( (j + 1) % no ))
-        count=$((count + 1))
-
-        for ((k = 0; k < no; k++))
-        do
-            printf "%d\t" "${frame[$k]}"
-        done
+    if [ $j -eq $numberOfFrames ]
+    then
+        memory[$memoryIndex]=${pages[$i]}
+        memoryIndex=$((memoryIndex+1))
+        pageFaultCount=$((pageFaultCount+1))
     fi
+
+    for ((k = 0; k < numberOfFrames; k++))
+    do
+        printf "\t%s" "${memory[$k]}"
+    done
+
+    if [ $j -eq $numberOfFrames ]
+    then
+        printf "\tPage Fault No: %d" $pageFaultCount
+    fi
+
     printf "\n"
+
+    if [ $memoryIndex -eq $numberOfFrames ]
+    then
+        memoryIndex=0
+    fi
 done
 
-printf "Total Page Faults: %d\n" "$count"
-
+printf "The number of Page Faults using FIFO is: %d\n" $pageFaultCount
